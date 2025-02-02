84cf3b60a2b2811287405e575a2a8d1295f09914
maziac <thomas.busse@gmx.net>
2025-02-02 18:29:32 +0100

    Adding maziac.zx81-bastop to container

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "install sjasmplus for the container"
+set -x
+
+# Prepare temporary directory
+cd /tmp
+mkdir -p codespaces-dezog-zx81
+cd  codespaces-dezog-zx81
+
+# Fetch sjasmplus
+wget -O  sjasmplus-1.20.3-src.tar.xz https://github.com/z00m128/sjasmplus/releases/download/v1.20.3/sjasmplus-1.20.3-src.tar.xz
+tar xf sjasmplus-1.20.3-src.tar.xz
+
+# Build
+cd sjasmplus-1.20.3
+make clean
+make
+sudo make install
+
+# Clean up
+rm -rf /tmp/codespaces-dezog-zx81
+
+
+echo "sjasmplus is installed"
